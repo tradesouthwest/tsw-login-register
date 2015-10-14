ba147a87a28561497b06c689ec2a6940fdfe9789
Larry <tradesouthwest@outlook.com>
2015-10-14 02:17:35 -0700

    remove public info
    clean text

--- register.php
+++ register.php
@@ -23,11 +23,11 @@ $title= 'TSW Login';
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <h1 class="panel-title text-center">Project Jaguar</h1>
+                    <h1 class="panel-title text-center">Tradesouthwest.com</h1>
                 </div>
  
                 <div class="col-lg-6">
-                    <h1 class="section-heading">Event Details</h1>
+                    <h1 class="section-heading">Website Details</h1>
                 
                 </div>
             </div><!-- /.row -->
